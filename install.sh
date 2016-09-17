28f59abbbf30e86027287cbb0d0cf55c8649bcf7
Cameron Taggart <cameron@tachyus.com>
2016-09-17 14:26:22 -0700

    fixed go get kubernetes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-go get -v -u golang.org/x/net/context
-go get -v -u google.golang.org/api/container/v1
-go get -v -u get k8s.io/client-go/1.4/kubernetes
+go get -u golang.org/x/net/context
+go get -u google.golang.org/api/container/v1
+go get -u k8s.io/client-go/1.4/kubernetes
