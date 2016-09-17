462f0360c25e5c934d7d0d5a733e7e90ece88188
Cameron Taggart <cameron@tachyus.com>
2016-09-17 14:38:15 -0700

    add compute metadata

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ go get -u google.golang.org/api/container/v1
 go get -u k8s.io/client-go/1.4/kubernetes
 go get -u google.golang.org/grpc
 go get -u golang.org/x/oauth2
+go get -u cloud.google.com/go/compute/metadata
